e9786ed5b1bc5a340b90318f610aa73b57eea0e5
gsio <gsio@protonmail.com>
2021-07-04 15:52:01 +0700

    Update register

--- lib/register.dart
+++ lib/register.dart
@@ -1,13 +1,24 @@
 class Register {
-  bool _registered = false;
+  static Set<Register> _registers = Set();
 
   void Function() initializer;
   Register(this.initializer);
 
   void call() {
-    if (!_registered) {
-      _registered = true;
+    if (!_registers.contains(this)) {
+      _registers.add(this);
       initializer();
     }
+  }
+
+  @override
+  int get hashCode => 0x43000000 | (initializer?.hashCode ?? 0);
+
+  @override
+  bool operator ==(Object other) {
+    if (other is Register) {
+      return initializer == other.initializer;
+    }
+    return super == other;
   }
 }
